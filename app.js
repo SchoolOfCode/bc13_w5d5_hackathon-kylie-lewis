6d810182acadc4f69bb45ab81cb09bb0a2e20890
L-E-W-1-5 <lewiswootton88@hotmail.com>
2022-10-28 09:46:06 +0100

    new file

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT;
 
 app.use(morgan("dev"));
 app.use(express.static("public"));
